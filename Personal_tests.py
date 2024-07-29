4a9b29336b8ae14e0d6ce1bccbf87a6e78e9d513
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2024-07-30 02:23:45 +0300

    ++

--- Personal_tests.py
+++ Personal_tests.py
@@ -1,3 +1,4 @@
+# %%
 try:
     import matplotlib.pyplot as plt
     print("module 'matplotlib' is installed")
@@ -22,5 +23,14 @@ class Circle(object):
         plt.axis('scaled')
         plt.show()
 
+# %%
 RedCircle = Circle(10, 'red')
+
+print('Radius of object:',RedCircle.radius)
+RedCircle.add_radius(2)
+print('Radius of object of after applying the method add_radius(2):',RedCircle.radius)
+RedCircle.add_radius(5)
+print('Radius of object of after applying the method add_radius(5):',RedCircle.radius)
+
 RedCircle.drawCircle()
+# %%
